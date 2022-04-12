e8d246514b8eb4af406e3df409535b0eb9869cfd
eswaribala <parameswaribala@gmail.com>
2022-04-12 18:13:10 +0530

    updated

---
+++ customerapi/src/main/java/com/citi/customerapi/CustomerapiApplication.java
@@ -0,0 +1,13 @@
+package com.citi.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerapiApplication.class, args);
+	}
+
+}
